7b4209f1764a20924a40f6ce9119ddae6edb84c5
Jennings Liu <jennings.liu@sap.com>
2024-07-04 16:52:14 +0800

    fix: add firt api with db connection

--- Makefile
+++ Makefile
@@ -2,24 +2,29 @@
 
 GO           ?= go
 GOFMT        ?= $(GO)fmt
+SQLC = ~/go/bin/sqlc
 FIRST_GOPATH := $(firstword $(subst :, ,$(shell $(GO) env GOPATH)))
 BIN_DIR ?= $(shell pwd)/build
 
 
 all:  fmt run
 
+sqlgen: |
+	@echo ">> generating db code"
+	$(SQLC) -f sqlc/sqlc.yaml  generate
 
-
-build: |
+build: | sqlgen
 	@echo ">> building binaries"
 	$(GO) build -o build/maco-deploy
 
 fmt:
 	@echo ">> format code style"
-	$(GOFMT) -w $$(find . -path ./vendor -prune -o -name '*.go' -print)
+	$(GOFMT) -w $$(find .  -path ./.history -prune -o -name '*.go' -print)
+
 run: build
 	@echo ">> start locally"
 	 build/maco-deploy
+
 clean:
 	rm -rf $(BIN_DIR)
 
--- main.go
+++ main.go
@@ -1,25 +1,72 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.wdf.sap.corp/maco-mmt/maco-deploy/db"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:passw0rd@127.0.0.1:5432/macodeploy?sslmode=disable"
 )
 
 func RootHandler(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index/index.html", "maco-deploy")
 
 }
-func UserHandler(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "user/index.html", nil)
+
+func GetUserInfoHandler(ctx *gin.Context) {
+	name := ctx.Param("username")
+	context := ctx.Request.Context()
+	dbConn, errDBconn := pgx.Connect(context, dbSource)
+	if errDBconn != nil {
+		log.Fatal("error connecting to db,", errDBconn)
+	}
+	query := db.New(dbConn)
+	user, errUer := query.GetUser(context, name)
+	if errUer != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"code": 404, "msg": "user not found"})
+
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{"code": 200, "result": user})
+	}
 
 }
+
+func GetUsersHandler(ctx *gin.Context) {
+	context := ctx.Request.Context()
+	dbConn, errDBconn := pgx.Connect(context, dbSource)
+	if errDBconn != nil {
+		log.Fatal("error connecting to db,", errDBconn)
+	}
+	query := db.New(dbConn)
+	users, errUer := query.GetUsers(context)
+	if errUer != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"code": 404, "msg": "user not found"})
+	} else {
+		if users == nil {
+			ctx.JSON(http.StatusOK, gin.H{"code": 200, "result": "{}"})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"code": 200, "result": users})
+		}
+
+	}
+
+}
+
 func main() {
 	//engine := gin.New()
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/**/*")
 	router.Static("/static", "static")
+
 	router.GET("/", RootHandler)
-	router.GET("/user", UserHandler)
+	router.GET("/user/*username", GetUserInfoHandler)
+	router.GET("/users", GetUsersHandler)
 	router.Run(":9000")
+
 }
