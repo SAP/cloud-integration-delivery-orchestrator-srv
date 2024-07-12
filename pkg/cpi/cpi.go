f57c36021cd5cf8c0a849d26c9e194534edb0c9f
Jennings Liu <jennings.liu@sap.com>
2024-07-12 12:07:29 +0800

    feat: implement getPackage

--- pkg/cpi/cpi.go
+++ pkg/cpi/cpi.go
@@ -10,7 +10,8 @@ import (
 )
 
 type CPIClient struct {
-	context context.Context
+	context     context.Context
+	HttpClient  *http.Client
 	AccessToken string
 	CpiAPI      string
 }
@@ -18,25 +19,28 @@ type CPIClient struct {
 type OauthResp struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
-	ExpiresIn     int    `json:"expires_in"`
+	ExpiresIn   int    `json:"expires_in"`
 	Scope       string `json:"scope"`
 	Jti         string `json:"jti"`
 }
 
-func NewCPIClient(ctx context.Context, clientID string, clientSecret string, cpiAuthURL string) *CPIClient {
-
+func NewCPIClient(ctx context.Context, clientID string, clientSecret string, cpiAuthURL string, cpiURL string) (*CPIClient, error) {
 	payload := strings.NewReader(fmt.Sprintf("grant_type=client_credentials&client_id=%s&client_secret=%s", clientID, clientSecret))
 
-	req, _ := http.NewRequest("POST", cpiAuthURL, payload)
+	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, cpiAuthURL, payload)
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
-	res, err := http.DefaultClient.Do(req)
+	httpClient := http.DefaultClient
+
+	res, err := httpClient.Do(req)
 	if err != nil {
 		fmt.Errorf("Error when get the response, %s", err)
+		return &CPIClient{}, err
 	}
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Errorf("Error when reading body from response, %s", err)
+		return &CPIClient{}, err
 
 	}
 
@@ -44,16 +48,86 @@ func NewCPIClient(ctx context.Context, clientID string, clientSecret string, cpi
 	jsonUnmarshalErr := json.Unmarshal(body, &oauthResp)
 	if jsonUnmarshalErr != nil {
 		fmt.Errorf("Error when extract jsib data from response, %s", jsonUnmarshalErr)
+		return &CPIClient{}, err
 	}
-
 	return &CPIClient{
-		context: ctx,
+		context:     ctx,
+		HttpClient:  httpClient,
 		AccessToken: oauthResp.AccessToken,
-		CpiAPI:      cpiAuthURL,
-	}
+		CpiAPI:      cpiURL,
+	}, nil
 }
 
-func ( c *CPIClient )  GetPackage() {
+type PackageResponseItem struct {
+	ID                string `json:"Id"`
+	Name              string `json:"Name"`
+	Description       string `json:"Description"`
+	ShortText         string `json:"ShortText"`
+	Version           string `json:"Version"`
+	Vendor            string `json:"Vendor"`
+	Mode              string `json:"Mode"`
+	SupportedPlatform string `json:"SupportedPlatform"`
+	ModifiedBy        string `json:"ModifiedBy"`
+	CreationDate      string `json:"CreationDate"`
+	ModifiedDate      string `json:"ModifiedDate"`
+	CreatedBy         string `json:"CreatedBy"`
+	Products          string `json:"Products"`
+	Keywords          string `json:"Keywords"`
+	Countries         string `json:"Countries"`
+	Industries        string `json:"Industries"`
+	LineOfBusiness    string `json:"LineOfBusiness"`
+}
 
+type PackageResponse struct {
+	D struct {
+		Results []PackageResponseItem `json:"results"`
+	} `json:"d"`
+}
+
+func (c *CPIClient) GetPackage() (PackageResponse, error) {
+	fullURL := fmt.Sprintf("%s%s", c.CpiAPI, "/IntegrationPackages")
+	fmt.Printf("Starting to get package from cpi tenant %s\n", fullURL)
+
+	childCtx, cancel := context.WithCancel(c.context)
+	defer cancel()
+	req, _ := http.NewRequestWithContext(childCtx, http.MethodGet, fullURL, nil)
+	tokenHeaderVal := fmt.Sprintf("Bearer %s", c.AccessToken)
+	req.Header.Add("Authorization", tokenHeaderVal)
+	req.Header.Add("Accept", "application/json")
+	resp, errReq := c.HttpClient.Do(req)
+	fmt.Printf("resp status code %d\n", resp.StatusCode)
+	if errReq != nil {
+		fmt.Errorf("Error when getting response from api, the error message is %s", errReq)
+		return PackageResponse{}, errReq
+	}
+
+	defer resp.Body.Close()
+
+	respBodyContent, errIOreader := io.ReadAll(resp.Body)
+
+	if errIOreader != nil {
+		fmt.Errorf("Error when getting content from response, error message %s", errIOreader)
+		return PackageResponse{}, errIOreader
+	}
+
+	var packcageResp PackageResponse
+	jsonUnmarshalError := json.Unmarshal(respBodyContent, &packcageResp)
+
+	if jsonUnmarshalError != nil {
+		fmt.Errorf("Error when unmarshal from json, error message %s", jsonUnmarshalError)
+		return PackageResponse{}, jsonUnmarshalError
+	}
+
+	return packcageResp, nil
+}
+
+func (c *CPIClient) GetIflows() {
+
+}
+
+func (c *CPIClient) CheckIflows() {
+
+}
+func (c *CPIClient) GetScripts() {
 
 }
---
+++ pkg/cpi/cpi_test.go
@@ -0,0 +1,23 @@
+package cpi
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCPIPackage(t *testing.T) {
+	ctx := context.Background()
+	clientID := "sb-8baf396e-a220-4936-a560-f16c898890b6!b486442|it!b410603"
+	clientSecret := "REDACTED"
+	cpiAuthURL := "https://cpi-mmt-preprod.authentication.eu10.hana.ondemand.com/oauth/token"
+	cpiURL := "https://cpi-mmt-preprod.it-cpi026.cfapps.eu10-002.hana.ondemand.com/api/v1"
+	client, err1 := NewCPIClient(ctx, clientID, clientSecret, cpiAuthURL, cpiURL)
+	if err1 != nil {
+		t.Fatalf("error when getting token, error message %s", err1)
+	}
+	packages, error2 := client.GetPackage()
+	if error2 != nil {
+		t.Fatalf("error when getting packages from cpi, %v\n  ", error2)
+	}
+	t.Logf("the packages in the cpi tenant: %v", packages)
+}
