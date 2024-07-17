4a72dc94bd93db4842be2b54f83d9fbbc259fb99
Jennings Liu <jennings.liu@sap.com>
2024-07-17 18:09:32 +0800

    feat: add db migrate

--- pkg/handler/dbconn.go
+++ pkg/handler/dbconn.go
@@ -2,9 +2,13 @@ package handler
 
 import (
 	"context"
-	"fmt"
-	"log"
+	"database/sql"
 
+	log "github.com/sirupsen/logrus"
+
+	"github.com/golang-migrate/migrate/v4"
+	pgxmig "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5"
 	"github.wdf.sap.corp/maco-mmt/maco-deploy/db"
 )
@@ -20,8 +24,7 @@ type DBClient struct {
 	Query  *db.Queries
 }
 
-func NewDBClient(ctx context.Context, driver string, dbHost string, dbPort string, dbUser string, dbPassword string, dbname string) (DBClient, error) {
-	dbSource := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbname)
+func NewDBClient(ctx context.Context) (DBClient, error) {
 	dbConn, errDBconn := pgx.Connect(ctx, dbSource)
 	if errDBconn != nil {
 		log.Fatal("error connecting to db,", errDBconn)
@@ -32,4 +35,30 @@ func NewDBClient(ctx context.Context, driver string, dbHost string, dbPort strin
 		DBConn: dbConn,
 		Query:  db.New(dbConn),
 	}, nil
+}
+
+func (d *DBClient) dbmigrate() {
+
+	pxConfig := &pgxmig.Config{
+		DatabaseName:    "macodeploy",
+		MigrationsTable: "user",
+	}
+	sqlInstance, error := sql.Open(dbDriver, dbSource)
+	if error != nil {
+		log.Fatalf("error when create db instance, error message is %s", error)
+	}
+
+	pxdriver, error2 := pgxmig.WithInstance(sqlInstance, pxConfig)
+
+	if error2 != nil {
+		log.Fatalf("error when px instance driver, error message is %s", error2)
+
+	}
+	migrator, error3 := migrate.NewWithDatabaseInstance("file://../db/migrations", "macodeploy", pxdriver)
+
+	if error3 != nil {
+		log.Fatalf("error when creating migratin instance, error message %s", error3)
+
+	}
+	migrator.Up()
 }
