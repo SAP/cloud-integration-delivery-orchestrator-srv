ac59a078a4616f6351096e124740ba34d3ca19f0
I589335 <doug.liu@sap.com>
2024-10-15 18:07:44 +0800

    fix: error log

--- pkg/oauth2/oauth.go
+++ pkg/oauth2/oauth.go
@@ -8,8 +8,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.wdf.sap.corp/maco-mmt/maco-deploy/env"
 )
 
 var client_id = "e413f654a5f193da8bed"
@@ -18,26 +19,30 @@ var client_secret = "REDACTED"
 func UserInfo(ctx *gin.Context) {
 	code := ctx.Query("code")
 	state := ctx.Query("state")
-	callbackUrl := ctx.Query("callback_url")
+	redirect_uri := ctx.Query("callbackUrl")
 	fmt.Println("state:", state)
 
 	token_url := "https://github.wdf.sap.corp/login/oauth/access_token"
-	payload := fmt.Sprintf("code=%s&redirect_uri=%s&client_id=%s&client_secret=%s", code, callbackUrl, client_id, client_secret)
+	payload := fmt.Sprintf("code=%s&redirect_uri=%s&client_id=%s&client_secret=%s", code, redirect_uri, client_id, client_secret)
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, token_url, strings.NewReader(payload))
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to create request of access token: " + err.Error()})
 		return
 	}
 	req.Header.Add("Accept", "application/json")
 	response, err := http.DefaultClient.Do(req)
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to get access token: " + err.Error()})
 		return
 	}
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to read response body: " + err.Error()})
 		return
 	}
 	var token TokenResponse
 	if err := json.Unmarshal(body, &token); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to unmarshal token: " + err.Error()})
 		return
 	}
 
@@ -45,25 +50,29 @@ func UserInfo(ctx *gin.Context) {
 	user_url := "https://github.wdf.sap.corp/api/v3/user"
 	req, err = http.NewRequestWithContext(ctx, http.MethodGet, user_url, nil)
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to create request of user info: " + err.Error()})
 		return
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
 	response, err = http.DefaultClient.Do(req)
 	if err != nil {
+		env.Logger().Error("Failed to get user info:", err)
 		return
 	}
 	body, err = io.ReadAll(response.Body)
 	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to read response body of user info: " + err.Error()})
 		return
 	}
 	var userInfo User
 	if err := json.Unmarshal(body, &userInfo); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to unmarshal user info: " + err.Error()})
 		return
 	}
 	session := sessions.Default(ctx)
 	// update session
 	session.Set("User", userInfo)
-	session.Save()
+	defer session.Save()
 	ctx.JSON(http.StatusOK, gin.H{"result": userInfo})
 }
 
